711d2797e1d7df97d7b8d6ea9cd707d0ccf6957e
Billy Chan <ccw.billy.123@gmail.com>
2023-12-07 17:19:55 +0800

    This not announced yet 🥷🏻

--- FireDBG/docusaurus.config.js
+++ FireDBG/docusaurus.config.js
@@ -144,6 +144,11 @@ module.exports = {
         background: '#AABBCC00',
       },
     },
+    announcementBar: {
+      id: 'firedbg-bar',
+      content: 'This not announced yet 🥷🏻',
+      isCloseable: false,
+    },
   },
   presets: [
     [
