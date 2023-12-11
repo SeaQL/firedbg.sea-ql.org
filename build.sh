c493853c406bbb9cb476ee651bcbb0898b94f760
Billy Chan <ccw.billy.123@gmail.com>
2023-12-11 14:44:01 +0800

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-[ -d dist ] && rm -r dist
+[[ -d dist ]] && rm -r dist
 
 mkdir dist
 
 cd FireDBG
 yarn install
 npx -y update-browserslist-db@latest
-[ -d build ] && rm -r build
+[[ -d build ]] && rm -r build
 yarn build
 mv build/* ../dist/
 
